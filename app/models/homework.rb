07d098544ef1c6bb5b76fad5f9e90c5bc0f9df55
mariangelica <mangedi@hotmail.com>
2015-04-25 16:31:57 +0000

    Validación de campos

--- app/models/homework.rb
+++ app/models/homework.rb
@@ -1,2 +1,14 @@
 class Homework < ActiveRecord::Base
+    belongs_to :team
+    
+    validates :title , presence: true
+    validates :description , presence: true
+    validates :duration , presence: true
+    validates :startdate , presence: true
+    validates :enddate , presence: true
+    validates :team_id , presence: true
+    validates :finished , presence: true
+    
+    validates :duration, numericality: { only_float: true }
+
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,6 @@
 class Team < ActiveRecord::Base
+    has_many :homeworks
+    validates :name , presence: true
+    validates :lastname , presence: true
+    validates :email , presence: true
 end
